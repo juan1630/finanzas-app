14c04fa2b80b13739940358ec0ecc4daba376106
Juan Patrón <josejuanpatron1630@gmail.com>
2025-03-09 22:12:13 -0600

    fixed viteConfig file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotnev from "dotenv";
 
 // https://vite.dev/config/
 export default ({ mode }) => {
@@ -10,7 +9,7 @@ export default ({ mode }) => {
     plugins: [react()],
     define: {
       //.env
-      "process.env.": env,
+      "process.env": env,
     },
   });
 };
