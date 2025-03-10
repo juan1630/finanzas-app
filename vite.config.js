297e1aec13743641f126be09b44dbf2d443fde48
Juan Patrón <josejuanpatron1630@gmail.com>
2025-03-09 22:21:37 -0600

    fixed prod

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,24 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default ({ mode }) => {
+export default ({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      //.env
-      "process.env": env,
-    },
-  });
+  if (command == "serve") {
+    return defineConfig({
+      plugins: [react()],
+      define: {
+        //.env
+        "process.env": JSON.stringify(env.local),
+      },
+    });
+  }else {
+    return defineConfig({
+      plugins: [react()],
+      define: {
+        //.env
+        "process.env": JSON.stringify(env.prod)
+      },
+    });
+  }
 };
