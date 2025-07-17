3c75408ab9c75e5720dd8d8b586aef21450eec24
Juan Patrón <josejuanpatron1630@gmail.com>
2025-07-16 20:37:52 -0600

    Fixed: login page and added tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default ({ mode, command }) => {
@@ -7,7 +8,7 @@ export default ({ mode, command }) => {
 
   if (command == "serve") {
     return defineConfig({
-      plugins: [react()],
+      plugins: [react(), tailwindcss()],
       define: {
         //.env
         base: env.VITE_API_URL,
